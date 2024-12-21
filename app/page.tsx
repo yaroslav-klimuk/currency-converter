758e880b0cb37ebe33fc656824b97b15a862b69b
yaroslav-klimuk <yaraslau.klimuk@gmail.com>
2024-12-21 19:12:06 +0100

    chore: correct paddings

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import ConverterWidget from "@/app/components/converter-widget"
 
 export default async function IndexPage() {
   return (
-    <section className="container flex flex-col items-center justify-center px-2 pb-8 pt-6 md:px-8 md:py-10">
+    <section className="container flex flex-col items-center justify-center px-4 pb-8 pt-6 md:px-8 md:py-10">
       <ConverterWidget />
     </section>
   )
